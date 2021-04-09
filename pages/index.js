6ad002486ba7a904b03d89b13abf2c4cdc8eb277
Nikhil Swaminathan <2429410+swaminator@users.noreply.github.com>
2021-04-09 11:13:38 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ const Index = () => (
             </Link>
           </li>
           <li>
-            <a href="/docs/page">Visit External URL</a>
+            <a href="/docs/page">Visit External URL (doesn't work)</a>
           </li>
         </ul>
       </nav>
